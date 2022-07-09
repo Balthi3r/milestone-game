efba663fc2924e6a47d5900b0828de14d19afa07
Balthier <rrodrijose@gmail.com>
2022-07-09 14:50:37 -0500

    working

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
+const start = () =>{
 const box = document.querySelectorAll('.box')
 const item = document.querySelector('.item')
 const time = document.querySelector('#time')
 const points = document.querySelector('#points')
 
 let result = 0
-let click
-let cTime=60
+let clicker
+let cTime=10
+
 
 function randomBox(){
     box.forEach(box =>{
         box.classList.remove('item')
     })
     let randomSpot= box[Math.floor(Math.random()*9)]
-    randomSpot.classlist.add('item')
-    click= randomBox.id
+    randomSpot.classList.add('item')
+
+    clicker = randomSpot.id
 }
     box.forEach(box => {
         box.addEventListener('click', () => {
-            if (box.id === click) {
+            if (box.id === clicker) {
                 result++
                 points.textContent = result
-                click = null
+                clicker = null 
             }
         })
     })
     
+    
 function moveItem(){
-    let timeId=null
     timeId= setInterval(randomBox, 1000)
 }
 //time function https://www.w3schools.com/js/js_timing.asp
 function countDown(){
-    cTime-1
+    cTime-=1
     time.textContent= cTime
-    if(cTime==o){
-        clearInterval(countDownTimerId)
+    if(cTime==0){
+        clearInterval(cTimeId)
         clearInterval(timeId)
         alert('TIME!!! You scored ' + result + ' points.')
     }
 }
 
-let cTimeID= setInterval(countDown, 1000)
+
+let cTimeId= setInterval(countDown, 1000)
 
 moveItem()
+}
